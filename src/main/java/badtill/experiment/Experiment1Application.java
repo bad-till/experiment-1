93174c6229d13d7ea105a3d68772527c32163969
till.krech <till@dasburo.com>
2017-02-11 16:32:22 +0100

    initial

---
+++ src/main/java/badtill/experiment/Experiment1Application.java
@@ -0,0 +1,12 @@
+package badtill.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiment1Application.class, args);
+	}
+}
